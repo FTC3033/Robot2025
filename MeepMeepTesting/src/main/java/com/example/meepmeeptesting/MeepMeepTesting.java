1881ece451644e374c781307b5ea9c87a9b59324
leal.philip <leal.philip@heb.com>
2025-10-20 11:11:59 -0500

    Adding MeepMeep

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,51 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        /*myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+                .lineToX(30)
+                .turn(Math.toRadians(90))
+                .lineToY(30)
+                .turn(Math.toRadians(90))
+                .lineToX(0)
+                .turn(Math.toRadians(90))
+                .lineToY(0)
+                .turn(Math.toRadians(90))
+                .build());*/
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, -13, 0))
+                .strafeToLinearHeading(new Vector2d(-23, -23), Math.toRadians(40))
+                .splineToLinearHeading(new Pose2d(35.0, -30.0, Math.toRadians(-90)), Math.toRadians(-90))
+                .lineToY(-60)
+                .lineToY(-34)
+                .strafeToLinearHeading(new Vector2d(-23, -23), Math.toRadians(40))
+                .splineToLinearHeading(new Pose2d(11.0, -30.0, Math.toRadians(-90)), Math.toRadians(-90))
+                .lineToY(-60)
+                .lineToY(-34)
+                .strafeToLinearHeading(new Vector2d(-23, -23), Math.toRadians(40))
+                .splineToLinearHeading(new Pose2d(-11.0, -30.0, Math.toRadians(-90)), Math.toRadians(-90))
+                .lineToY(-60)
+                .lineToY(-34)
+                .strafeToLinearHeading(new Vector2d(-23, -23), Math.toRadians(40))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
