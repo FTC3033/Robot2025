9a196c3eaff36eb56e596a02fdbd002fe1e0510c
sykevrc <jwei2295@stu.neisd.net>
2025-10-20 11:02:15 -0500

    Auto1 Works, Moving on to shooter pid for consistency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
@@ -11,14 +11,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name="Auto1", group="Robot")
 public class Auto1 extends LinearOpMode  {
     public static double DISTANCE = 64;
 
-    private DcMotor shooterR;
-    private DcMotor shooterL;
+    private DcMotorEx shooterR;
+    private DcMotorEx shooterL;
     private DcMotor intakeMotor;
     private DcMotor passer;
     private CRServo passerServo;
@@ -30,8 +31,8 @@ public class Auto1 extends LinearOpMode  {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
 
         intakeMotor = hardwareMap.get(DcMotor.class, "intaker");
-        shooterL = hardwareMap.get(DcMotor.class, "shooterL");
-        shooterR = hardwareMap.get(DcMotor.class, "shooterR");
+        shooterL = hardwareMap.get(DcMotorEx.class, "shooterL");
+        shooterR = hardwareMap.get(DcMotorEx.class, "shooterR");
         shooterR.setDirection(DcMotorSimple.Direction.REVERSE);
         passer = hardwareMap.get(DcMotor.class, "Mpasser");
         passerServo = hardwareMap.get(CRServo.class, "passer");
@@ -39,35 +40,39 @@ public class Auto1 extends LinearOpMode  {
         waitForStart();
 
         Actions.runBlocking(drive.actionBuilder(new Pose2d(0,0,0))
-                        .lineToX(DISTANCE)
+                        //.lineToX(DISTANCE)
                         //.splineTo(new Vector2d(DISTANCE, 0), Math.toRadians(180))
                         //.splineToLinearHeading(new Pose2d(-3, 0, 0), Math.PI / 2)
-                        .stopAndAdd(new ShooterAction(shooterR, shooterL, false))
-                        .waitSeconds(1)
+                        .stopAndAdd(new ShooterAction(shooterR, shooterL, 0.75,false))
                         .stopAndAdd(new PasserAction(passer, passerServo, false))
-                        .waitSeconds(2)
-                        .stopAndAdd(new PasserAction(passer, passerServo, true))
-                        .stopAndAdd(new ShooterAction(shooterR, shooterL, true))
-                        .lineToX(0)
+//                        .waitSeconds(2)
+//                        .stopAndAdd(new PasserAction(passer, passerServo, true))
+//                        .stopAndAdd(new ShooterAction(shooterR, shooterL, true))
+//                        .lineToX(0)
                         .stopAndAdd(new IntakeAction(intakeMotor, false))
-                        .waitSeconds(2)
-                        .stopAndAdd(new IntakeAction(intakeMotor, true))
+                        .waitSeconds(10)
+
+//                        .waitSeconds(2)
+//                        .stopAndAdd(new IntakeAction(intakeMotor, true))
                         .build());
 
     }
 
     public class ShooterAction implements Action {
 
-        DcMotor shooterMotor1;
-        DcMotor shooterMotor2;
+        DcMotorEx shooterMotor1;
+        DcMotorEx shooterMotor2;
 
-
+        private double velocity;
+        private final int P = 2;
+        private final int ticks = 28;
         boolean finished = false;
 
-        ShooterAction(DcMotor shooterMotor1, DcMotor shooterMotor2, boolean finished) {
+        ShooterAction(DcMotorEx shooterMotor1, DcMotorEx shooterMotor2, double velocity, boolean finished) {
             this.shooterMotor1 = shooterMotor1;
             this.shooterMotor2 = shooterMotor2;
             this.finished = finished;
+            this.velocity = velocity;
         }
 
         @Override
@@ -77,12 +82,14 @@ public class Auto1 extends LinearOpMode  {
                 shooterMotor1.setPower(0.0);
                 shooterMotor2.setPower(0.0);
             } else {
-                shooterMotor1.setPower(.75);
-                shooterMotor2.setPower(.75);
+                shooterMotor1.setPower(P*(Math.abs(velocity*2800-shooterMotor1.getVelocity())/(velocity*28)));
+                shooterMotor2.setPower(P*(Math.abs(velocity*2800-shooterMotor2.getVelocity())/(velocity*28)));
+                double vel = shooterMotor1.getVelocity();//2800 usually
+                telemetryPacket.put("V1", vel);
             }
 
 
-            return false;
+            return true;
         }
     }
 
